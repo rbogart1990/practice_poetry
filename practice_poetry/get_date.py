5a2b94bf5310cb0592fff7f9497d77153dd1f8b5
RobertABogart <robertino.bogart1@accesscfa.com>
2024-07-10 18:03:31 -0400

    get first day of quarter

--- practice_poetry/get_date.py
+++ practice_poetry/get_date.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import logging
 import os
+import pendulum
 
 # Set up logging
 logging.basicConfig(
@@ -14,4 +15,12 @@ LOG = logging.getLogger(os.path.basename(__file__))
 # Get current date
 current_datetime = datetime.now()
 current_date = current_datetime.date()
-LOG.info(f'current_date: {current_date}')
+current_date_str = current_date.strftime('%Y-%m-%d')
+LOG.info(f'current_date: {current_date_str}')
+
+# Get first day of current quarter
+p = pendulum.from_format(current_date_str, 'YYYY-MM-DD')
+first_of_quarter = p.first_of('quarter').strftime('%Y-%m-%d')
+# Convert to datetime.date object:
+first_of_quarter = datetime.strptime(first_of_quarter, '%Y-%m-%d').date()
+LOG.info(f'first_of_quarter: {first_of_quarter}')
